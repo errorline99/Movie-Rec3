a371f74f09ccdf64cf222e3b9eb5d912ff88a4ef
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-05-18 21:48:32 +0300

    Update switch.js

--- src/app/components/switch.js
+++ src/app/components/switch.js
@@ -4,13 +4,7 @@
 import { useEffect } from "react"
 
 export default function Pull(props) { 
-        useEffect(()=> {
-        function zoom() {
-            document.body.style.zoom = "125%" 
-        }
 
-        zoom()
-    })
     return(<>
 
     </>)
