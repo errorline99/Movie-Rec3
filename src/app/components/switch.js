e630f8d824674ac5198b455112636463507a314c
errorline99 <100209216+errorline99@users.noreply.github.com>
2024-05-18 20:39:52 +0300

    Update switch.js

--- src/app/components/switch.js
+++ src/app/components/switch.js
@@ -3,6 +3,13 @@
 
 
 export default function Pull(props) { 
+        useEffect(()=> {
+        function zoom() {
+            document.body.style.zoom = "125%" 
+        }
+
+        zoom()
+    })
     return(<>
 
     </>)
